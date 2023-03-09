790d70bd02215cc6dde8e02df3907eb44c341946
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-03-09 16:12:44 +0100

    Inserita logica scroll page

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,18 +1,69 @@
 <script>
     export default {
-        name: 'AppHeader'
+        name: 'AppHeader',
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            openDroopdown() {
+                const dropContent = document.querySelector('.menu .dropdown-content');
+                if ( dropContent.classList.contains('d-block') ) {
+                    dropContent.classList.remove('d-block');
+                } else {
+                    dropContent.classList.add('d-block');
+                }
+            },
+            scrollPage(action) {
+                switch (action) {
+                    case 'toTop':
+                        window.scroll({
+                            top: 0,
+                            behavior: 'smooth'
+                        })
+                        break;
+                    case 'toProjects':
+                        const projects = document.querySelector('#homepage__projects');
+                        console.log(projects.offsetHeight)
+
+                        window.scroll({
+                            top: 650,
+                            behavior: 'smooth'
+                        })
+                        break;
+                    case 'toContacts':
+                        // Get max scroll height page
+                        const limit = Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
+                        
+                        window.scroll({
+                            top: limit,
+                            behavior: 'smooth'
+                        })
+                        break;
+                }
+            }
+        }
     }
 </script>
 
 <template>
     <header>
         <div class="ms-container d-flex justify-content-between">
-            <router-link :to="{ name: 'homepage' }" class="text-decoration-none text-white">
-                My portfolio
+            <router-link :to="{ name: 'homepage' }" class="text-decoration-none text-white" @click="scrollPage('toTop')">
+                <span class="title-portfolio m-text-cursive">Portfolio</span>
             </router-link>
 
             <nav class="menu">
-                link
+                <div class="dropdown">
+                    <button @click="openDroopdown()">
+                        <i class="fa-solid fa-bars"></i>
+                    </button>
+                    <div class="dropdown-content">
+                        <div @click="scrollPage('toProjects')">Projects</div>
+                        <div @click="scrollPage('toContacts')">Contacts</div>
+                    </div>
+                </div>
             </nav>
         </div>
     </header>
@@ -30,5 +81,34 @@
         line-height: 70px;
         background-color: black;
         color: white;
+    }
+    header .title-portfolio {
+        font-size: 30px;
+    }
+
+    /* DROPDOWN MENU */
+    .dropdown {
+        display: inline-block;
+        position: relative;
+    }
+    .dropdown button {
+        border: none;
+        border: 0.5px solid white;
+        padding: 10px 20px;
+        background-color: transparent;
+        color: white;
+        cursor: pointer;
+    }
+    .dropdown-content {
+        width: 200px;
+        text-align: center;
+        display: none;
+        position: absolute;
+        right: 0;
+        overflow: auto;
+        background-color: white;
+        color: black;
+        box-shadow: 0px 10px 10px 0px rgba(0,0,0,0.4);
+        z-index: 10;
     }
 </style>
